307586162fb2c4db81eee7a930cfcd75fe98fbf7
Nikita Danilenko <nda@informatik.uni-kiel.de>
2014-07-30 13:23:12 +0200

    Aufgeräumt und ein wenig kommentiert

--- src/controller/Pair.java
+++ src/controller/Pair.java
@@ -1,26 +1,61 @@
 package controller;
 
+/**
+ * Repräsentiert Paare mit beliebigen Typen in den verschiedenen Komponenten.
+ * 
+ * @author nda
+ *
+ * @param <A>
+ * @param <B>
+ */
+
 public class Pair<A, B> {
 
-	A first;
-	B second;
+	private A first;
+	private B second;
+	
+	/**
+	 * Konstruiert ein neues Paar aus zwei gegebenen Objekten.
+	 * @param first erste Komponente
+	 * @param second zweite Komponente
+	 */
 	
 	public Pair(A first, B second){
 		this.first = first;
 		this.second = second;
 	}
 	
+	/**
+	 * 
+	 * @return liefert die erste Komponente
+	 */
+	
 	public A getFirst(){
 		return first;
 	}
+	
+	/**
+	 * 
+	 * @return liefert die zweite Komponente
+	 */
 	
 	public B getSecond(){
 		return second;
 	}
 	
+	/**
+	 * Setzt die erste Komponente auf den gegebenen Wert (destruktiv).
+	 * @param first
+	 */
+	
 	public void setFirst(A first){
 		this.first = first;
 	}
+	
+	/**
+	 * Setzt die zweite Komponente auf den gegebenen Wert (destruktiv).
+	 * @param second
+	 */
 	
 	public void setSecond(B second){
 		this.second = second;
--- src/controller/Position.java
+++ src/controller/Position.java
@@ -1,32 +1,67 @@
 package controller;
 
-/**@author nda*/
+/**
+ * Positionen sind Paare von ganzzahligen Koordinaten. Da int als Typ nicht in
+ * Typvariablen vorkommen darf, wird intern überall Integer verwendet.
+ * 
+ * @author nda*/
 
 public class Position {
 
     private Pair<Integer, Integer> values;
+   
+    
+    /**
+     * Konstruiert eine neue Position aus zwei ganzen Zahlen.
+     * @param x horizontal
+     * @param y vertikal
+     */
     
     public Position(int x, int y){
         values = new Pair<Integer, Integer>(new Integer(x), new Integer(y));
     }
     
+    /**
+     * Setzt die x-Koordinate auf den neuen Wert (destruktiv).
+     * @param x
+     */
+    
     public void setX(int x){
         values.setFirst(new Integer(x));
     }
+    
+    /**
+     * Setzt die y-Koordinate auf den neuen Wert (destruktiv).
+     * @param y
+     */
     
     public void setY(int y){
         values.setSecond(new Integer(y));
     }
     
+    /**
+     * Liefert die x-Koordinate.
+     * @return
+     */
+    
     public int getX(){
         return values.getFirst().intValue();
     }
+    
+    /**
+     * Liefert die y-Koordinate.
+     * @return
+     */
     
     public int getY(){
         return values.getSecond().intValue();
     }
     
+    /**
+     * Es wird das zugrundeliegende Paar angezeigt.
+     */
+    
     public String toString(){
         return values.toString();
     }
-}
+
